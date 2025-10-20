1390336a953a48e01084c751f9f5ee3d635b8f01
Masum Prodhan <masumbillahprodhan003@gmail.com>
2025-10-20 11:30:54 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+import Navbar from './component/Navbar/Navbar'
+function App() {
+  
+
+  return (
+    <>
+     <Navbar></Navbar>
+      
+      <div className="main-container">
+        
+        <div className="left-container w-[70] border-amber-100">
+
+        </div>
+
+        <div className="right-container w-[30] border-zinc-800">
+
+        </div>
+        </div> 
+     
+    </>
+  )
+}
+
+export default App
